cf66506b841d644fd2b57fe5082691dfdaee850a
Beatriz de Souza Santos <biasouza1400@gmail.com>
2022-02-16 22:26:25 -0300

    Exercicios de interpretação concluidos

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,18 @@
+// ### EXERCICIOS DE INTERPRETAÇÃO DE CÓDIGO ###
+/*
+1 - a) O que vai ser impresso no console?
+    No primeiro console será impresso o número 10, já que a multiplicação será de 2 * 5
+    No segundo  console será impresso o número 50, já que a multiplicação será de 10 * 5
+
+    b)O que aconteceria se retirasse os dois console.log e simplesmente invocasse a função minhaFuncao(2) e minhaFuncao(10)? O que apareceria no console?
+    Não iria aparecer nada, a função funcionaria normalmente, mas como não estamos a chamando ela não iria retornar o resultado da 
+    multiplicação para o usuário ou o programador visualizar.
+
+2 - a) Explique o que essa função faz e qual é sua utilidade 
+    A função irá pegar um texto digitado pelo usuário e vai deixar todas as letras minusculas, através do toLoweCase() e depois
+    irá ver se possui a palavra "cenoura" na frase, a partir do includes().
+
+    b)true ; true; false.
+    
+
+*/
