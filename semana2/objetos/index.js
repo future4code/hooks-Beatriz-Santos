405381d3c2e7c15730e15797d213ceff918f3025
Beatriz de Souza Santos <biasouza1400@gmail.com>
2022-02-22 00:25:39 -0300

    Exercicios de interpretacao, concluidos

---
+++ semana2/objetos/index.js
@@ -0,0 +1,19 @@
+// ### EXERCICIO DE INTERPRETAÇÃO ###
+/* 1 - a)
+PRIMEIRO CONSOLE = Matheus Nachtergaele
+SEGUNDO  CONSOLE = Virginia Cavendish
+TERCEIRO CONSOLE = Globo  14h
+
+2 - a)
+PRIMEIRO CONSOLE = nome: "Juca", idade: 3, raca: "SRD"
+SEGUNDO  CONSOLE = nome: "Juba", idade: 3, raca: "SRC"
+TERCEIRO CONSOLE = nome: "Jubo", idade: 3, raca: "SRD"
+
+b) Os três pontos antes do nome do objeto serve para repeti-lo, assim conseguimos repetir as informações e adicionar ou alterar os dados
+
+3 - a)
+PRIMEIRO CONSOLE = false
+SEGUNDO  CONSOLE = undefined - indefinido
+
+b) A função irá procurar a propriedade dentro do objeto, se encontrá-la irá retornar o valor atribuido, caso não encontre irá retornar como indefinido.
+*/
