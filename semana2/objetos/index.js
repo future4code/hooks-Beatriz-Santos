4dc3ab699088a48d30a6eddf39f4a4e299a4387e
Beatriz de Souza Santos <biasouza1400@gmail.com>
2022-02-22 22:53:19 -0300

    Exercicios de escrita, concluidos.

--- semana2/objetos/index.js
+++ semana2/objetos/index.js
@@ -17,3 +17,54 @@ SEGUNDO  CONSOLE = undefined - indefinido
 
 b) A função irá procurar a propriedade dentro do objeto, se encontrá-la irá retornar o valor atribuido, caso não encontre irá retornar como indefinido.
 */
+// ### EXERCICIO DE ESCRITA ###
+// --- 1 ---
+//a)
+const exibirPessoa = objeto =>{
+    return `Eu sou ${objeto.nome}, mas pode me chamar de: ${objeto.apelido[0]}, ${objeto.apelido[1]} ou ${objeto.apelido[2]}`;
+}
+
+const pessoa = {
+    nome: 'Beatriz',
+    apelido: ['Bia', 'Bea', 'Bibi']
+}
+
+console.log(exibirPessoa(pessoa));
+
+//b)
+
+const novoApelido = {
+    ...pessoa,
+    apelido: ['Bi', 'xuxu', 'meu bem']
+}
+console.log(exibirPessoa(novoApelido));
+
+// --- 2 ---
+//a)
+const dadosPessoa = {
+    nome: 'Heloisa',
+    idade: 14,
+    profissao: 'estudante'
+}
+//b)
+const exibirDados = dados =>{
+    const informações = [dados.nome, dados.nome.length, dados.idade, dados.profissao, dados.profissao.length];
+
+    return informações;
+}
+console.log(exibirDados(dadosPessoa));
+
+// --- 3 ---
+const adicionarCarrinho = frutas =>{
+    carrinho.push(fruta);
+    return carrinho;
+}
+
+let carrinho = [];
+const fruta = [
+    {nome: 'maça',   disponibilidade: true},
+    {nome: 'melao',  disponibilidade: true},
+    {nome: 'banana', disponibilidade: true}
+]
+
+console.log(adicionarCarrinho(fruta));
