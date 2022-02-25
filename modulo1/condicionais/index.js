28ffddc91e7b96f32d735f1753ec77ebf0341bc9
Beatriz de Souza Santos <biasouza1400@gmail.com>
2022-02-24 21:01:02 -0300

    Exercicios de interpretação, concluidos

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,19 @@
+// ### EXERCICIO DE INTERPRETAÇÃO ###
+/* --- 1 --
+a) O código irá pegar um número digitado pelo usuário e dividir por dois, e verificar se o resto da divisão é igual a 0(zero), ou seja,
+   é um código para verificar se um número é PAR ou ÍMPAR.
+b) Caso imprima 'Passou no teste' o número é PAR, já que o resto da divisão é ZERO
+c) Caso imprima 'Não passou no teste' o número é ÍMPAR, já que o resto da divisão é diferente de ZERO
+
+--- 2 ---
+a) O código irá pedir o nome de uma fruta para o usuário e devolver o nome da fruta e o valor dela no console
+b) Se o usuário digitar MAÇA o resultado será, "O preço da fruta Maça é R$ 2.25"
+c) Ele irá retorna o valor R$ 5, já que irá atribuir o valor do default ao preço
+
+--- 3 ---
+a) A primeira linha está atribuindo o valor digitado pelo usuário a uma variavel chamada 'numero' e a declarando como um NUMBER (tipo númerico)
+b) Caso o numero digitado seja 10 ele irá retornar a mensagem 'Este número passou no teste' e criar uma variavel chamada mensagem. 
+   Caso o número digitado seja -10 ele irá retornar um erro.
+c) Sim daria erro, pois a variavel mensagem foi declarada dentro do IF e só pode ser utilizada dentro daquele bloco ou nos blocos 'filhos' do mesmo,
+   então quando chamada fora do IF irá retornar um erro, já que não é uma váriavel global e sim local.
+*/
