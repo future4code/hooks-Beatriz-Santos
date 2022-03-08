dd90122241c67f73e06e8f12b3c2d8a0b97ff0e5
Beatriz de Souza Santos <biasouza1400@gmail.com>
2022-03-08 20:18:40 -0300

    Exercicios de interpretação, concluidos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,11 @@
+/* ### EXERCICIOS DE INTERPRETAÇÃO ###
+
+1 - O for está rodando 4 vezes e está somando o valor que está armazenado na variavel 'valor' mais o valor de 'i'
+O valor exibido no console será 10.
+
+2 - a) 19, 21, 23, 25, 27, 30 - Esses serão os valores impressos no console
+    b)  
+
+3 - a) Irá imprimir a seguinte linha ' * ** *** **** '
+
+*/
