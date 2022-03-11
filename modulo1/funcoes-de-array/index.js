305499b7626154b2495f586977b55d6388fe2fca
Beatriz de Souza Santos <biasouza1400@gmail.com>
2022-03-10 21:41:00 -0300

    Exercicios de interpretacao, concluidos

---
+++ modulo1/funcoes-de-array/index.js
@@ -0,0 +1,9 @@
+// ### EXERCICIOS DE INTERPRETACAO ###
+/*
+1 - Irá retornar o valor do primeiro item do array, em seguida o indice desse objeto e por ultimo irá imprimir o array inteiro. Isso irá se repetir com todos os itens do array
+
+2 - Irá imprimir somente os valores armazenados na chave de nome 'nome'.
+
+3 - No console aparecerá os valores: 'Mandi' e 'Laura', já que a condição do filtro é que o apelido tenha que ser diferente de Chijo
+ */
+
