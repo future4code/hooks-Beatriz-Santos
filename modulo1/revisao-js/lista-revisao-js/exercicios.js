bcfeb208937c0b68eb57f55d37dd93811c2eb5dd
Beatriz de Souza Santos <biasouza1400@gmail.com>
2022-03-18 21:06:34 -0300

    Exercicio 05, concluido

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -4,32 +4,41 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   return array.length;
+    return array.length;
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  return array.reverse();
+    return array.reverse();
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  return array.sort((a, b) => {
-        return a - b
-  })
+    return array.sort((a, b) => {
+        return a - b;
+    });
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  let arrayPar = array.filter((item) => {
-    return item = item % 2 == 0
-  });
-  return arrayPar;
+    let arrayPar = array.filter((item) => {
+        return item = item % 2 == 0;
+    });
+
+    return arrayPar;
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+    let pares = array.filter((item) => {
+        return item = item % 2 == 0;
+    });
+
+    let elevadoADois = pares.map((item) => {
+        return Math.pow(item, 2);
+    });
+
+    return elevadoADois;
 }
 
 // EXERCÍCIO 06
