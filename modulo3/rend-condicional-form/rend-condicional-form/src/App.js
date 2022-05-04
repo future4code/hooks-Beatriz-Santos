e235cf49984097602a701b581436ed947b530c1e
Beatriz de Souza Santos <biasouza1400@gmail.com>
2022-05-03 23:12:28 -0300

    Mudança de pagina, concluido

--- modulo3/rend-condicional-form/rend-condicional-form/src/App.js
+++ modulo3/rend-condicional-form/rend-condicional-form/src/App.js
@@ -8,11 +8,36 @@ import FinalFormulario from './components/FinalFormulario/FinalFormulario';
 
 
 class App extends React.Component { 
-
+  state = {
+    proximaPagina: 0
+   }
+ 
+  mudarPagina = () =>{
+    this.setState({proximaPagina: this.state.proximaPagina + 1 })
+  }
+ 
+  paginaSeguinte = () =>{
+      switch(this.state.proximaPagina){
+       case 0:
+         return <DadosGerais mudarEstado={this.mudarPagina}/>
+       
+       case 1:
+         return <InformacaoEnsino mudarEstado={this.mudarPagina}/>
+       
+       case 2:
+         return <InformacaoEnsinoSuperior mudarEstado={this.mudarPagina}/>
+       
+       case 3:
+         return <FinalFormulario />
+ 
+       default:
+         return 'Página não encontrada :('
+      }
+  }
   render(){
     return (
       <>
-      
+        {this.paginaSeguinte()}
       </>
     );
   }
