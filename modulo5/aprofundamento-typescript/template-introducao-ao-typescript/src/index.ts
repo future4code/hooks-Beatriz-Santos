59510a3565ab11c785aed87cdb119b6c7c06754c
Beatriz de Souza Santos <biasouza1400@gmail.com>
2022-08-11 23:36:01 -0300

    Exercicio 02 concluido

--- modulo5/aprofundamento-typescript/template-introducao-ao-typescript/src/index.ts
+++ modulo5/aprofundamento-typescript/template-introducao-ao-typescript/src/index.ts
@@ -2,8 +2,10 @@
 console.log("============= EXERCICIO 01 ===============")
 const minhaString: string = 'Beatriz';
 //Quando atribuido um valor diferente de string dá erro e o programa não roda.
+
 const numero: number | string = 5;
 // Para aceitar mais de um tipo utilizamos o pipe (|)
+
 enum corFavorita{
     VERMELHO = "Vermelho",
     VIOLETA = "Violeta",
@@ -25,3 +27,32 @@ const pessoa01: Pessoa = {
     corFavorita: corFavorita.VIOLETA
 }
 console.log(pessoa01)
+
+// ============= EXERCICIO 02 ===============
+console.log('============= EXERCICIO 02 ===============')
+
+const obterEstatisticas = (numeros: Array<number>) => {
+
+    const numerosOrdenados = numeros.sort (
+        (a: number, b:number) => a - b
+    )
+
+    let soma: number = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
+
+console.log(obterEstatisticas([7, 8, 5, 3]))
+
+// ============= EXERCICIO 03 ===============
+console.log('============= EXERCICIO 03 ===============')
