bc3811d28be8cc5a7848f7cea1de83371ddecaaf
Beatriz de Souza Santos <biasouza1400@gmail.com>
2022-09-13 17:57:54 -0300

    Exercicio01, concluido

---
+++ modulo6/introducao-sql/Exercicio01.sql
@@ -0,0 +1,9 @@
+-- Active: 1659394409919@@35.226.146.116@3306@hooks-4313186-beatriz-santos
+USE `hooks-4313186-beatriz-santos`;
+SET SQL_SAFE_UPDATES = 0;
+/* ----- EXERCICIO 01 ------*/
+CREATE TABLE Funcionarios(
+	id VARCHAR(3),
+    nome VARCHAR(30),
+    email VARCHAR(50)
+);
---
+++ modulo6/introducao-sql/Exercicio02.sql
@@ -0,0 +1,4 @@
+-- Active: 1659394409919@@35.226.146.116@3306@hooks-4313186-beatriz-santos
+USE `hooks-4313186-beatriz-santos`;
+
+SELECT * FROM `Funcionarios`;
